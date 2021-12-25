89209635ef6423112d566b5c247e9422c4e69c3d
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2021-12-25 20:48:07 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,25 @@
-#include <exception>
-#include <fstream>
-#include <iostream>
-#include <regex>
-#include <string>
 #include "config_handler.h"
-#include "workflow_executor.h"
 
-int main(int argc, char** argv) {
-    try {
-        if (argc != 2) {
-            throw WorkflowException("Bad number of args");
-        }
-        ConfigHandler config_handler(argv[1]);
-        WorkflowExecutor& workflow_executor = config_handler.ProcessConfigFile();
-        workflow_executor.Execute();
-    }
+int main(int argc, char* argv[1]) {
+	string file_name = "workflow.txt";
 
-    catch (WorkerException& exc1) {
-        std::cout << exc1.what() << std::endl;
-        return 1;
-    }
-    catch (WorkflowException& exc2) {
-        std::cout << exc2.what() << std::endl;
-        return 1;
-    }
-    return 0;
+	try {
+		if (argc != 2) {
+			throw WorkflowException("Bad number of args");
+		}
+		file_name = argv[1];
+		ConfExecutor ex(file_name);
+		ex.Execute();
+	}
+
+	catch (WorkerException& exc1) {
+		std::cout << exc1.what() << std::endl;
+		return 1;
+	}
+	catch (WorkflowException& exc2) {
+		std::cout << exc2.what() << std::endl;
+		return 1;
+	}
+
+	return 0;
 }
