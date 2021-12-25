a91ad4d64ca7bfb6c9f051eb3934f34bbbdbbea3
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2021-12-25 12:16:04 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,26 @@
 #include <iostream>
 #include <regex>
 #include <string>
-
 #include "config_handler.h"
+#include "workflow_executor.h"
 
 int main(int argc, char** argv) {
-  std::string file_name{"test.txt"};
-  if (argc == 2) {
-    file_name = argv[1];
-  }
-  ConfigHandler config_handler(file_name);
-  WorkflowExecutor& workflow_executor = config_handler.ProcessConfigFile();
-  workflow_executor.Execute();
-  return 0;
+    try {
+        if (argc != 2) {
+            throw WorkflowException("Bad number of args");
+        }
+        ConfigHandler config_handler(argv[1]);
+        WorkflowExecutor& workflow_executor = config_handler.ProcessConfigFile();
+        workflow_executor.Execute();
+    }
+
+    catch (WorkerException& exc1) {
+        std::cout << exc1.what() << std::endl;
+        return 1;
+    }
+    catch (WorkflowException& exc2) {
+        std::cout << exc2.what() << std::endl;
+        return 1;
+    }
+    return 0;
 }
