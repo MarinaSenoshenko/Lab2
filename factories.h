ded557338ef94948c90241e88e2a6504f4ceedb9
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2021-12-26 15:08:37 +0700

    Update factories.h

--- factories.h
+++ factories.h
@@ -8,68 +8,74 @@ using namespace std;
 
 
 class Worker {
-public:
-	virtual void Execute() = 0;
-	virtual pair<bool, bool> GetIO() const = 0;
+    public:
+	    virtual void Execute() = 0;
+	    virtual pair<bool, bool> GetIO() const = 0;
 };
+
 
 class Reader : public Worker {
-protected:
-	const string fileName;
-	vector<string>* data;
-public:
-	Reader(vector<string>* data, const string fileName = "");
-	virtual void Execute();
-	virtual pair<bool, bool> GetIO() const;
+    protected:
+		vector<string>* data;
+	    const string file_name;
+    public:
+	    Reader(vector<string>* data, const string file_name = "");
+	    virtual void Execute();
+	    virtual pair<bool, bool> GetIO() const;
 };
+
 
 class Grepper : public Worker {
-private:
-	vector<string>* data;
-	vector<string> new_data;
-	const string word;
-public:
-	Grepper(vector<string>* data, const string word = "");
-	virtual void Execute();
-	virtual pair<bool, bool> GetIO() const;
+    private:
+	    vector<string>* data;
+	    vector<string> new_data;
+	    const string word;
+    public:
+	    Grepper(vector<string>* data, const string word = "");
+	    virtual void Execute();
+	    virtual pair<bool, bool> GetIO() const;
 };
+
 
 class Writer : public Worker {
-private:
-	string fileName;
-	vector<string>* data;
-public:
-	Writer(vector<string>* data, string fileName = "");
-	virtual void Execute();
-	virtual pair<bool, bool> GetIO() const;
+    private:
+		vector<string>* data;
+	    string file_name;
+    public:
+	    Writer(vector<string>* data, string file_name = "");
+	    virtual void Execute();
+	    virtual pair<bool, bool> GetIO() const;
 };
+
 
 class Sorter : public Worker {
-private:
-	vector<string>* data;
-public:
-	Sorter(vector<string>* data);
-	virtual void Execute();
-	virtual pair<bool, bool> GetIO() const;
+    private:
+	    vector<string>* data;
+    public:
+	    Sorter(vector<string>* data);
+	    virtual void Execute();
+	    virtual pair<bool, bool> GetIO() const;
 };
+
 
 class Replacer : public Worker {
-private:
-	vector<string>* data;
-	const string word1;
-	const string word2;
-public:
-	Replacer(vector<string>* data, const string word1 = "", const string word2 = "");
-	virtual void Execute();
-	virtual pair<bool, bool> GetIO() const;
+    private:
+	    vector<string>* data;
+	    const string word1;
+	    const string word2;
+    public:
+	    Replacer(vector<string>* data, const string word1 = "", const string word2 = "");
+	    virtual void Execute();
+	    virtual pair<bool, bool> GetIO() const;
 };
 
+
 class Dumper : public Worker {
-private:
-	const string fileName;
-	vector<string>* data;
-public:
-	Dumper(vector<string>* data, const string fileName = "");
-	virtual void Execute();
-	virtual pair<bool, bool> GetIO() const;
+    private:
+		vector<string>* data;
+	    const string file_name;
+    public:
+	    Dumper(vector<string>* data, const string file_name = "");
+	    virtual void Execute();
+	   virtual pair<bool, bool> GetIO() const;
 };
