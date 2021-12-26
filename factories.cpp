d2bc64851ec9cc69eb8f15f4cbf6bc573a7adfab
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2021-12-26 15:08:08 +0700

    Update factories.cpp

--- factories.cpp
+++ factories.cpp
@@ -1,12 +1,13 @@
-#include "factories.h"
 #include <fstream>
+#include <string>
+#include "factories.h"
 
-Reader::Reader(vector<string>* data, const string fileName) : data(data), fileName(fileName) {}
+Reader::Reader(vector<string>* data, const string file_name) : data(data), file_name(file_name) {}
 Grepper::Grepper(vector<string>* data, const string word) : data(data), word(word) {}
 Sorter::Sorter(vector<string>* data) : data(data) {}
 Replacer::Replacer(vector<string>* data, const string word1, const string word2) : data(data), word1(word1), word2(word2) {}
-Writer::Writer(vector<string>* data, string fileName) : data(data), fileName(fileName) {}
-
+Writer::Writer(vector<string>* data, string file_name) : data(data), file_name(file_name) {}
+Dumper::Dumper(vector<string>* data, const string file_name) : data(data), file_name(file_name) {}
 
 
 pair<bool, bool> Reader::GetIO() const {
@@ -24,12 +25,15 @@ pair<bool, bool> Replacer::GetIO() const {
 pair<bool, bool> Writer::GetIO() const {
 	return make_pair(true, false);
 }
+pair<bool, bool> Dumper::GetIO() const {
+	return make_pair(true, true);
+}
 
 
 void Reader::Execute() {
-	ifstream input(fileName);
+	ifstream input(file_name);
 	if (!input) {
-		throw FileException(fileName);
+		throw FileException(file_name);
 	}
 	string buffer;
 	while (!input.eof()) {
@@ -43,33 +47,42 @@ void Grepper::Execute() {
 	for (auto& it : *data) {
 		int pos = it.find(word);
 		string buffer = it;
-		if (pos >= 0)
+		if (pos >= 0) {
 			new_data.push_back(it);
+		}
 	}
 	*data = new_data;
 }
 
 void Sorter::Execute() {
 	std::sort(data->begin(), data->end());
-
 }
 
 void Replacer::Execute() {
 	for (auto& it : *data) {
 		int pos = it.find(word1);
-		if (pos >= 0)
+		if (pos >= 0) {
 			it.replace(pos, word1.length(), word2);
+		}
 	}
 }
 
 void Writer::Execute() {
-	ofstream output(fileName);
-	if (!output)
-		throw FileException(fileName);
-
-	for (auto& it : *data)
+	ofstream output(file_name);
+	if (!output) {
+		throw FileException(file_name);
+	}
+	for (auto& it : *data) {
 		output << it;
+	}
 }
 
-
-
+void Dumper::Execute() {
+	ofstream output(file_name);
+	if (!output) {
+		throw FileException(file_name);
+	}
+	for (auto& it : *data) {
+		output << it;
+	}
+}
